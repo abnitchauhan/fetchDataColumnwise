e67b180f4b6b6e51b434f7cee5823a8a71824749
Abnit Chauhan <abnitchauhanabnit@gmail.com>
2021-03-15 10:54:59 +0530

    Read the Data Columnwise using PHP

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php 
+
+$csv = 'data.csv';
+
+$file = fopen($csv, 'r');
+
+while(($readcsv = fgetcsv($file)) !== FALSE)
+{
+    $data[] = $readcsv;  //Read the Data Rowise Initially
+}
+
+for($i=0;$i<count($data); $i++)
+    {
+        $columnwise[] = getColumn($data, $i); // Call the getColumn function 
+    }
+
+// Function To Read the Data Columwise
+function getColumn($arr, $col)
+{
+    $column = [];
+    for($i=0;$i<count($arr);$i++)
+        {  
+        $column[] = $arr[$i][$col];//Removed Floatval 
+        } 
+    return $column;
+}  
+
+echo "ColumnWise Data <br/>";
+echo json_encode($columnwise); // Print the Data Columwise
+
+
+
+
+?>
